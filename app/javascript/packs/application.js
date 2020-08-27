8fa1eadcf37229f0f8c0970bc9bfdb7220c69f98
10susumu <s.i.19921029@gmail.com>
2020-08-27 17:03:56 +0900

    コメントアウト解除

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 require('jquery')
 require("../time")
-// require("../sound")
+require("../sound")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
--- app/javascript/sound.js
+++ app/javascript/sound.js
@@ -1,10 +1,10 @@
 window.addEventListener('load',() =>{
   document.getElementById('target').addEventListener('click', () => {
+    document.getElementById( 'sound-file' ).play() ;
     if (document.getElementById('target').style.background === 'pink'){
       document.getElementById('target').style.background = 'lightblue';
     }else{
       document.getElementById('target').style.background = 'pink';
     }
-    document.getElementById( 'sound-file' ).play() ;
   });
 });
