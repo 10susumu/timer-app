3847b56e099c6dfd61b20d3ab87a7b2189a3a7b2
10susumu <s.i.19921029@gmail.com>
2020-08-27 18:17:16 +0900

    Update time.js

--- app/javascript/time.js
+++ app/javascript/time.js
@@ -11,16 +11,16 @@ const timer = () => {
   document.getElementById("now-time").innerHTML = hhmm;
   
   const forms = document.getElementsByClassName("set-times");
-  const maxnum = forms.item(0).childElementCount - 1;
-  const firstnum = forms.item(0).children.item(0).getAttribute("id")
-  const lastnum = forms.item(0).children.item(maxnum).getAttribute("id")
-  for (let i = firstnum; i <= (lastnum); i++){
-    const form = document.getElementsByClassName("set-time"+i);
-    if (form.item(0).getAttribute("value") != null ){
-      if (form.item(0).getAttribute("value") == hhmm ){
+  const maxnum = forms.item(0).childElementCount;
+  
+  
+  
+  for (let i = 0; i < maxnum; i++){
+    const form = forms.item(0).children.item(i).getAttribute("value");
+    console.log(form)
+      if (form == hhmm ){
         document.getElementById( 'sound-file' ).play() ;
       } else {
-      }
     }
   }
 }
